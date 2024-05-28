6aba49b552bc813a7cb7e417b30e283ca2ba8174
Evgeny Eysner <eeysner@dataahead.de>
2024-05-28 10:57:07 +0200

    fix: cart request

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import basicSsl from '@vitejs/plugin-basic-ssl'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+    base: /frontend/,
     server: {
         proxy: {
             // '/api': 'http://127.0.0.1:8000',
