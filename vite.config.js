626f0901f6cd312e192552a240c57a9d22dc6c41
Evgeny Eysner <eeysner@dataahead.de>
2024-05-28 12:10:29 +0200

    feat: rename root directory

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import basicSsl from '@vitejs/plugin-basic-ssl'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    // base: '/',
+    base: '/frontend/',
     server: {
         proxy: {
             // '/api': 'http://127.0.0.1:8000',
