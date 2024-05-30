b13d6b123f79be9dbe14fa3a43e83fa97ea73689
Evgeny Eysner <eeysner@dataahead.de>
2024-05-30 13:19:59 +0200

    feat: new build

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,6 @@ import {VitePWA} from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: '/',
     server: {
         proxy: {
             // '/api': 'http://127.0.0.1:8000',
