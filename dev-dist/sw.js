c0b0566a9508e2fb52bfcc2dc7ba0145fa9eb65b
crasavella04 <andreyberd04@gmail.com>
2024-08-10 13:29:44 +0300

    change barcode lib, remove other libs

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-b5f7729d'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.q19hke23h2o"
+    "revision": "0.rsh2vvhpph"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
