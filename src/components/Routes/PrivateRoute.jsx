3b6a63163b3015e819768f1a5c20b8ccc8ae91a5
Evgeny Eysner <eeysner@dataahead.de>
2024-12-11 17:47:11 +0100

    fix: private route

--- src/components/Routes/PrivateRoute.jsx
+++ src/components/Routes/PrivateRoute.jsx
@@ -1,20 +1,3 @@
-// import { Navigate, Outlet } from 'react-router-dom'
-// import { useAuthStore } from '../../store/auth'
-//
-// export const PrivateRoute = () => {
-//   const { isAuth, isTokenValid, logout } = useAuthStore()
-//
-//   // Check if user is authenticated and token is valid
-//   if (!isAuth || !isTokenValid()) {
-//     logout() // Log out user if the token is invalid
-//     return <Navigate to='/login' replace />
-//   }
-//
-//   return <Outlet />
-// }
-
-
-import { useEffect } from 'react';
 import { Navigate, Outlet } from 'react-router-dom';
 import { useAuthStore } from '../../store/auth';
 import useLogout from '../../hooks/useLogout';
@@ -22,17 +5,11 @@ import useLogout from '../../hooks/useLogout';
 export const PrivateRoute = () => {
   const { isAuth, isTokenValid } = useAuthStore();
   const logout = useLogout();
-  const shouldLogout = !isAuth || !isTokenValid();
 
-  useEffect(() => {
-    if (shouldLogout) {
-      logout(); // Log out and navigate only after the component has finished rendering
-      return <Navigate to='/login' replace />
-    }
-  }, [shouldLogout, logout]);
-
-  if (shouldLogout) {
-    return null; // Render nothing while `logout` is handled in `useEffect`
+  // Check if the user should be logged out
+  if (!isAuth || !isTokenValid()) {
+    logout(); // Log out the user synchronously
+    return <Navigate to="/login" replace />;
   }
 
   return <Outlet />;
